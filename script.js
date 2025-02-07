52ef0c3e1895577868995bb953e08e406f40297e
Ken Hamada <kenhamada@Kens-Mac-mini.local>
2025-02-07 10:27:04 -0800

    Fixed modal auto-launching, game loading, and broken image

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // Open the game modal
-function openModal(game) {
-    document.getElementById("game-modal").style.display = "none";
-    document.getElementById("game-frame").src = "games/dodge-the-creeps/index.html"; // Loads the game
+function openModal() {
+    var modal = document.getElementById("game-modal");
+    var iframe = document.getElementById("gameFrame");
+
+    modal.style.display = "flex"; // Show the modal
+    iframe.src = "games/dodge-the-creeps/index.html"; // Load the game
 }
 
 // Close the game modal
 function closeModal() {
-    document.getElementById("game-modal").style.display = "none";
-    document.getElementById("game-frame").src = ""; // Stops the game when closing
+    var modal = document.getElementById("game-modal");
+    var iframe = document.getElementById("gameFrame");
+
+    modal.style.display = "none"; // Hide the modal
+    iframe.src = ""; // Clear the game to stop it when closing
 }
